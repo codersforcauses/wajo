cadc6f59d1646f3242e6af59f4d3aeaa2c358e38
Yunho Ding <gurumindc123@gmail.com>
2024-11-29 21:10:22 +0800

    Add production support and update environment configurations
    - Enhance README with production mode instructions
    - Modify entrypoint scripts to handle production environment
    - Update Dockerfile to create access logs directory
    - Change Django settings module path in ASGI and WSGI files
    - Adjust static file paths in Django settings
    - Introduce docker-compose configuration for production
    - Add script to run development servers for client and server

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -39,9 +39,9 @@ fi
 # Run Django/Gunicorn
 # ===================
 if [ "${APP_ENV^^}" = "PRODUCTION" ]; then
-
+    su -
     # Run Gunicorn / Django
     printf "\n" && echo " Running Gunicorn / Django"
     echo "Running: gunicorn api.wsgi -b 0.0.0.0:8000 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50"
-    gunicorn api.wsgi -b 0.0.0.0:8000 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50
+    poetry run gunicorn api.wsgi -b 0.0.0.0:8000 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50
 fi
