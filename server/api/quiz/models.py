befdd01ffd743ef7a39421147ed6e11cc62d82d4
Alan <alanlam0802@gmail.com>
2025-01-04 06:09:28 +0000

    implemented the quiz attempt and quiz attempt user models

--- server/api/quiz/models.py
+++ server/api/quiz/models.py
@@ -29,3 +29,31 @@ class QuizSlots(models.Model):
     display_number = models.CharField()
     require_previous = models.BooleanField(default=False)
     block = models.IntegerField()
+
+
+class QuizAttempt(models.Model):
+    id = models.AutoField(primary_key=True)
+    quiz_id = models.ForeignKey("QuizSlots", on_delete=models.CASCADE)
+    attempt = models.IntegerField()
+    question_attempts = models.IntegerField()
+    current_page = models.IntegerField()
+    state = models.CharField(max_length=10)
+    time_start = models.DateTimeField(auto_now_add=True)
+    time_finish = models.DateTimeField(auto_now_add=True)
+    time_modified = models.DateTimeField(auto_now=True)
+    time_modified_offline = models.DateTimeField(auto_now=True)
+    sum_grades = models.DecimalField(max_digits=10, decimal_places=5)
+
+    def __str__(self):
+        return f"{self.id} {self.quiz_id} {self.attempt}"
+
+
+class QuizAttemptUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    quiz_attempt = models.ForeignKey("QuizAttempt", on_delete=models.CASCADE)
+    student = models.ForeignKey("QuizAttempt", on_delete=models.CASCADE)
+    grade = models.DecimalField(max_digits=10, decimal_places=5)
+    time_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} {self.quiz_attempt} {self.student}"
