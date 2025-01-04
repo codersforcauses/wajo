8c121cefc0c02a8f0ef9b114c50d6ed509b93a9f
Alan <alanlam0802@gmail.com>
2025-01-04 06:10:51 +0000

    added the admin interface for the quiz models

--- server/api/quiz/admin.py
+++ server/api/quiz/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import QuizAttempt
+from .models import QuizAttemptUser
+
+
+class QuizAttemptAdmin(admin.ModelAdmin):
+    list_display = ("id", "quiz_id", "attempt")
+
+
+class QuizAttemptUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "quiz_attempt", "student")
+
+
+admin.site.register(QuizAttempt, QuizAttemptAdmin)
+admin.site.register(QuizAttemptUser, QuizAttemptUserAdmin)
