5443092c89f72f1dc715fcf790118e3cb8928528
AarifLamat <aarif.lamat@outlook.com>
2025-01-08 03:20:26 +0000

    added lil headings

--- client/src/pages/quiz/competition/[id].tsx
+++ client/src/pages/quiz/competition/[id].tsx
@@ -1,8 +1,11 @@
 import GenericQuiz from "@/components/ui/generic-quiz";
 
 export default function CompetitionQuizPage() {
+  // this value will be fetched from the database
+  let competitionName = "Maths Competition";
   return (
     <div className="flex min-h-dvh w-full items-center justify-center border-2 border-orange-600">
+      <h1>{competitionName}</h1>
       <GenericQuiz />
     </div>
   );
--- client/src/pages/quiz/practice/[id].tsx
+++ client/src/pages/quiz/practice/[id].tsx
@@ -1,8 +1,11 @@
 import GenericQuiz from "@/components/ui/generic-quiz";
 
 export default function PracticeQuizPage() {
+  // this value will be fetched from the database
+  let practiceName = "Practice Quiz";
   return (
     <div className="flex min-h-dvh w-full items-center justify-center border-2 border-orange-600">
+      <h1>{practiceName}</h1>
       <GenericQuiz />
     </div>
   );
