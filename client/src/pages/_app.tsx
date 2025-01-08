04997bcacccf76ba57bf84f9ee926569d9854b1b
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2025-01-08 10:13:38 +0800

    prettier formatting

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { Roboto,Urbanist } from "next/font/google";
+import { Roboto, Urbanist } from "next/font/google";
 import type { ReactElement, ReactNode } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
