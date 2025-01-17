40edda440ebe18ed154594d0d56c30ce6a7156df
Alan <alanlam0802@gmail.com>
2025-01-17 14:15:09 +0000

    added more serializers for quiz model

--- server/api/quiz/serializers.py
+++ server/api/quiz/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Quiz
+from .models import Quiz, QuizAttempt, QuizAttemptUser
 
 
 class QuizSerializer(serializers.ModelSerializer):
@@ -15,4 +15,36 @@ class QuizSerializer(serializers.ModelSerializer):
             'open_time_date',
             'close_time_date',
             'timelimit'
+        ]
+
+
+class QuizAttemptSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuizAttempt
+        fields = [
+            'id',
+            'quiz_id',
+            'attempt',
+            'question_attempts',
+            'current_page',
+            'state',
+            'time_start',
+            'time_finish',
+            'time_modified',
+            'time_modified_offline',
+            'sum_grades'
+        ]
+
+
+class QuizAttemptUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuizAttemptUser
+        fields = [
+            'id',
+            'quiz_attempt',
+            'student_id',
+            'grade',
+            'time_modified'
         ]
