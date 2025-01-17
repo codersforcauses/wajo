a1935633db0930cf8a2012f5717ce7bc23d74344
Alan <alanlam0802@gmail.com>
2025-01-17 15:52:12 +0000

    added basic views for QuizAttempt and Quiz AttemptUser model

--- server/api/quiz/views.py
+++ server/api/quiz/views.py
@@ -1,11 +1,93 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Quiz
-from .serializers import QuizSerializer
+from rest_framework import status
+from .models import Quiz, QuizAttempt, QuizAttemptUser
+from .serializers import QuizSerializer, QuizAttemptSerializer, QuizAttemptUserSerializer
+from django.http import Http404
 
 
 class QuizListView(APIView):
-    def get(self, request):
+    """
+    RETRIEVE ALL THE QUIZES OR CREATE A QUIZ
+    """
+
+    def get(self, request, format=None):
         quizzes = Quiz.objects.all()
         serializer = QuizSerializer(quizzes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = QuizSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuizAttemptList(APIView):
+    """
+    RETRIEVE ALL THE QUIZ ATTEMPTS
+    """
+
+    def get(self, request, format=None):
+        quizAttempt = QuizAttempt.objects.all()
+        serializer = QuizAttemptSerializer(quizAttempt, many=True)
+        return Response(serializer.data)
+
+
+class QuizAttemptDetail(APIView):
+    """
+    RETRIEVE UPDATE OR DELETE A QUIZ ATTEMPT
+    """
+
+    def get_object(self, pk):
+        try:
+            return QuizAttempt.objects.get(pk=pk)
+        except QuizAttempt.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        quizAttempt = self.get_object(pk)
+        serializer = QuizAttemptSerializer(quizAttempt)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        quizAttempt = self.get_object(pk)
+        serializer = QuizAttemptSerializer(quizAttempt, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        quizAttempt = self.get_object(pk)
+        quizAttempt.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class QuizAttemptUserList(APIView):
+    """
+    RETRIEVE ALL QUIZ ATTEMPT USERS
+    """
+
+    def get(self, request, format=None):
+        quizAttemptUser = QuizAttemptUser.objects.all()
+        serializer = QuizAttemptUserSerializer(quizAttemptUser, many=True)
+        return Response(serializer.data)
+
+
+class QuizAttemptUserDetail(APIView):
+    """
+    GET A QUIZ ATTEMPT USER
+    """
+
+    def get_object(self, pk):
+        try:
+            return QuizAttemptUser.objects.get(pk=pk)
+        except QuizAttemptUser.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        QuizAttemptUser = self.get_object(pk)
+        serializer = QuizAttemptUserSerializer(QuizAttemptUser)
         return Response(serializer.data)
