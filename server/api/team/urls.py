aa6b94ba008766ff3c6f723297441a2ccfb2ddb9
ErikaKK <491649804@qq.com>
2025-01-18 12:14:29 +0800

    edit urls in team app

--- server/api/team/urls.py
+++ server/api/team/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeamMemberViewSet, TeamViewSet
+from .views import TeamViewSet
 from . import views
 
+# app_name = "team"
 router = DefaultRouter()
 router.register(r'', TeamViewSet)
-router.register(r'member', TeamMemberViewSet)
+# router.register(r'member', TeamMemberViewSet)
 
 urlpatterns = [
-    # Handles all basic CRUD for teams and team members
+    path('member/', views.member_list, name='member-list'),
+
     path('', include(router.urls)),
 
     # For specific team details
     path('<int:pk>/', views.team_detail, name='team-detail'),
-    #     path('member/', views.member_list, name='member-list'),
     path('member/<int:pk>/', views.team_member_detail,
          name='team-member-detail'),  # For specific team member details
 ]
