9d27a00db66c4c0e05dadfda415e30421e85593f
thnorton <thnorto1@gmail.com>
2025-01-18 03:51:23 +0000

    added back client

--- server/api/question/urls.py
+++ server/api/question/urls.py
@@ -1,14 +1,10 @@
 
 from django.urls import path
 from .views import CategoryView, QuestionView, AnswerView
-from api.question import views
 
 
 app_name = "question"
 urlpatterns = [
-    # path("get/", views.question_list, name="question-list"),
-    # path("create/", views.create_question, name="create-question"),
-    # path("answer/create/", views.create_answer, name="create-answer"),
     path("questions/", QuestionView.as_view(), name="question"),
     path("questions/<int:id>/", QuestionView.as_view(), name="question-id"),
 
