90501030ffa6549d864cbda918a17d31a9ab6a59
Philip Wu <troubledphilip@gmail.com>
2025-01-18 08:59:56 +0000

    scheduler optimization

--- server/api/quiz/signals.py
+++ server/api/quiz/signals.py
@@ -8,7 +8,7 @@ from apscheduler.jobstores.base import JobLookupError
 @receiver(post_save, sender=Quiz)
 def update_quiz_tasks(sender, instance, created, **kwargs):
     try:
-        # 尝试移除任务，如果任务不存在，捕获异常
+        # remove existing jobs
         scheduler.remove_job(f"start-{instance.id}")
     except JobLookupError:
         print(f"No start job found for Quiz {instance.id}, skipping removal.")
@@ -18,7 +18,7 @@ def update_quiz_tasks(sender, instance, created, **kwargs):
     except JobLookupError:
         print(f"No end job found for Quiz {instance.id}, skipping removal.")
 
-    # 添加新任务
+    # create new jobs
     if instance.status == 'upcoming':
         scheduler.add_job(
             start_quiz,
