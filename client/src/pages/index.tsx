88bfc27e3855ee6f1bea0e23200b940bf4162e9b
ErikaKK <491649804@qq.com>
2025-01-18 11:22:23 +0800

    initial commit

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -13,7 +13,30 @@ const Home = () => {
   });
 
   return (
-    <main className="font-urbanist flex min-h-screen flex-col items-center gap-4 p-24">
+    <main className="font-urbanist flex min-h-screen flex-col items-center gap-4">
+      <div
+        className="flex h-96 w-full flex-col items-center justify-between pb-16 pt-36"
+        style={{
+          background:
+            "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 59%, rgba(224,224,224,1) 100%)",
+        }}
+      >
+        <h3>WAJO Olympiads will be back next year!</h3>
+        <Button>Start Quiz</Button>
+      </div>
+      <div className="flex flex-col items-center gap-4 px-32">
+        <h4>What is WAJO and what are the Olympiads?</h4>
+        <div className="body-2">
+          Western Australian Junior mathematics Olympiad is an annual
+          competition held within WA to help identity and celebrate the
+          brightest mathematical minds amongst our youth. The competition is
+          open to high school students in years 7, 8 and 9 (Occasionally, a
+          younger exceptional student may arise, who you may wish to
+          participate; find out more here). The Olympiad consists of 2 parts: 1)
+          The individual paper which will be held on this website and 2) a team
+          paper, held in person.
+        </div>
+      </div>
       <h1>Test title</h1>
       <h2>Test title</h2>
       <h3>Test title</h3>
