cb609266b3d3b882a86c143894aef8365a87d1c3
Yunho Ding <gurumindc123@gmail.com>
2025-01-21 11:28:47 +0000

    enhance serializers and views with detailed docstrings; add comprehensive tests for question and category functionality

--- server/api/question/serializers.py
+++ server/api/question/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import Question, Category
 from api.users.serializers import UserSerializer
 
-
 class CategorySerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Category model.
+
+    Attributes:
+        genre (CharField): The genre of the category.
+        info (CharField): Additional information about the category.
+    """
     genre = serializers.CharField(max_length=50, required=True)
-    # info is long text field
     info = serializers.CharField(required=False)
 
     class Meta:
@@ -14,6 +19,18 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Question model.
+
+    Attributes:
+        name (CharField): The name of the question.
+        created_by (UserSerializer): The user who created the question.
+        modified_by (UserSerializer): The user who modified the question.
+        category_ids (PrimaryKeyRelatedField): The categories associated with the question.
+        categories (CategorySerializer): The categories associated with the question.
+        is_comp (BooleanField): Indicates if the question is for competition.
+        answer (ListField): The correct answer to the question.
+    """
     name = serializers.CharField(max_length=255, required=True)
     created_by = UserSerializer(read_only=True).fields['username']
     modified_by = UserSerializer(read_only=True).fields['username']
@@ -25,6 +42,15 @@ class QuestionSerializer(serializers.ModelSerializer):
         child=serializers.IntegerField(), default=[1])
 
     def create(self, validated_data):
+        """
+        Create a new Question instance.
+
+        Args:
+            validated_data (dict): The validated data for the question.
+
+        Returns:
+            Question: The created question instance.
+        """
         request = self.context.get('request')
         validated_data['created_by'] = request.user
         validated_data['modified_by'] = request.user
--- server/api/question/views.py
+++ server/api/question/views.py
@@ -7,9 +7,18 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from django.utils.timezone import now
 
-
 @permission_classes([IsAdminUser])
 class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Question instances.
+
+    Attributes:
+        queryset (QuerySet): The queryset of Question instances.
+        serializer_class (Serializer): The serializer class for Question instances.
+        filter_backends (list): The filter backends for the viewset.
+        search_fields (list): The fields to search in the viewset.
+        filterset_fields (list): The fields to filter in the viewset.
+    """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
@@ -17,6 +26,15 @@ class QuestionViewSet(viewsets.ModelViewSet):
     filterset_fields = ['mark']
 
     def create(self, request, *args, **kwargs):
+        """
+        Create a new Question instance.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: The response object.
+        """
         # validate integrity of name
         name = request.data.get('name')
         if Question.objects.filter(name=name).exists():
@@ -24,29 +42,71 @@ class QuestionViewSet(viewsets.ModelViewSet):
         return super().create(request, *args, **kwargs)
 
     def perform_update(self, serializer):
+        """
+        Update an existing Question instance.
+
+        Args:
+            serializer (Serializer): The serializer object.
+        """
         instance = serializer.save(modified_by=self.request.user)
         instance.time_modified = now()
         instance.save()
 
     @action(detail=False, methods=['get'])
     def search_by_answer(self, request):
+        """
+        Search questions by answer.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: The response object.
+        """
         self.search_fields = ['answer']
         return self.list(request)
 
     # @action(detail=False, methods=['get'])
     # def get_random_question(self, request):
+    #     """
+    #     Get a random question.
+    #
+    #     Args:
+    #         request (Request): The request object.
+    #
+    #     Returns:
+    #         Response: The response object.
+    #     """
     #     question = Question.objects.order_by("?").first()
     #     serializer = self.get_serializer(question)
     #     return Response(serializer.data)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Category instances.
+
+    Attributes:
+        queryset (QuerySet): The queryset of Category instances.
+        serializer_class (Serializer): The serializer class for Category instances.
+        filter_backends (list): The filter backends for the viewset.
+        search_fields (list): The fields to search in the viewset.
+    """
     queryset = Category.objects.all().order_by("id")
     serializer_class = CategorySerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['name']
 
     def create(self, request, *args, **kwargs):
+        """
+        Create a new Category instance.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: The response object.
+        """
         # validate integrity of genre
         genre = request.data.get('genre')
         if Category.objects.filter(genre=genre).exists():
