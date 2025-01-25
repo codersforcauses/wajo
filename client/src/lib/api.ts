25697060838f3bae9667898da806100d7d8c0c52
Lok Yx <loklokyiuxuan@gmail.com>
2025-01-25 03:37:07 +0000

    feat(auth): add custom 'role' field to JWT token, reconnect client-server
    - feat(auth): include 'role' field in JWT token for role-based access control
    - refactor(client-server): replace client-side mock API with proper server communication
    - chore: ensure seamless integration between client and server endpoints

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,10 +2,7 @@ import axios from "axios";
 
 export const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL_BASE;
 
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api" // temporarily use port 3000 in dev for mock data
-    : process.env.NEXT_PUBLIC_BACKEND_URL;
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const api = axios.create({ baseURL });
 
