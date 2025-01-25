c3a57a67c1f00728f965a90832092764e74bf5ce
Yunho <gurumindc123@gmail.com>
2025-01-25 17:11:20 +0000

    reverse change

--- server/api/settings.py
+++ server/api/settings.py
@@ -71,12 +71,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-<<<<<<< HEAD
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
-=======
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
->>>>>>> 6dc9b0c (feat: add pagination settings to REST framework configuration)
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 SIMPLE_JWT = {
