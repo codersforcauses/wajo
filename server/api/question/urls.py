da4fb78139ecc7d0490f3b839e8edc6aab49d85b
Yunho Ding <gurumindc123@gmail.com>
2025-01-29 11:53:46 +0000

    refactor: remove AnswerViewSet import from question URLs to streamline code

--- server/api/question/urls.py
+++ server/api/question/urls.py
@@ -1,6 +1,6 @@
 # from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewSet, CategoryViewSet, AnswerViewSet
+from .views import QuestionViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'question-bank', QuestionViewSet, basename='question-bank')
