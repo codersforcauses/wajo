c063d34fc47be413ab3abbc6e78be5662f2281e8
Yunho Ding <gurumindc123@gmail.com>
2025-01-29 08:31:56 +0000

    refactor: remove unused UserSerializer import from question serializers

--- server/api/question/serializers.py
+++ server/api/question/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Question, Category, Answer
-from api.users.serializers import UserSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
