e25ad1353275647ca63f90bb30cd741078bd0898
Yunho Ding <gurumindc123@gmail.com>
2025-01-30 13:38:48 +0000

    feat: enhance token serializer to include school and subscription IDs; update view and URL references

--- server/api/auth/serializers.py
+++ server/api/auth/serializers.py
@@ -6,14 +6,22 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user: User):
         role = "admin" if user.is_staff else None
+        school_id = None
+        sub_id = None
 
         if hasattr(user, "student"):
             role = "student"
+            school_id = user.student.school.id
+            sub_id = user.student.id
         elif hasattr(user, "teacher"):
             role = "teacher"
+            school_id = user.teacher.school.id
+            sub_id = user.teacher.id
 
         token = super().get_token(user)
         token['is_superuser'] = user.is_superuser
         token['role'] = role
+        token['school_id'] = school_id
+        token['sub_id'] = sub_id
 
         return token
--- server/api/users/views.py
+++ server/api/users/views.py
@@ -2,7 +2,8 @@ from django.db import IntegrityError
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
-from rest_framework.permissions import IsAuthenticated, IsAdminUser  # ,allowAny
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAdminUser)
 from django.contrib.auth.models import User
 from .models import Student, Teacher, School
 from .serializers import StudentSerializer, SchoolSerializer, TeacherSerializer, UserSerializer
@@ -88,7 +89,7 @@ class StudentViewSet(viewsets.ModelViewSet):
             )
 
 
-@permission_classes([IsAdminUser])
+@permission_classes([IsAuthenticated])
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
@@ -96,6 +97,21 @@ class TeacherViewSet(viewsets.ModelViewSet):
     filterset_fields = ['school']
     search_fields = ['user__username']
 
+    def retrieve(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super().retrieve(request, *args, **kwargs)
+        elif hasattr(request.user, "teacher"):
+            print(kwargs["pk"])
+            if request.user.teacher.id == int(kwargs["pk"]):
+                return super().retrieve(request, *args, **kwargs)
+            else:
+                return Response(
+                    {"error": "You do not have permission to access this resource."}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            return Response(
+                {"error": "You do not have permission to access this resource."}, status=status.HTTP_403_FORBIDDEN)
+
+    @permission_classes([IsAdminUser])
     def create(self, request, *args, **kwargs):
         try:
             return super().create(request, *args, **kwargs)
@@ -105,14 +121,19 @@ class TeacherViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
+    @permission_classes([IsAuthenticated])
     def update(self, request, *args, **kwargs):
-        try:
+        if request.user.is_staff:
             return super().update(request, *args, **kwargs)
-        except IntegrityError as error:
+        elif hasattr(request.user, "teacher"):
+            if request.user.teacher.id == int(kwargs["pk"]):
+                return super().update(request, *args, **kwargs)
+            else:
+                return Response(
+                    {"error": "You do not have permission to access this resource."}, status=status.HTTP_403_FORBIDDEN)
+        else:
             return Response(
-                {"error": str(error)},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+                {"error": "You do not have permission to access this resource."}, status=status.HTTP_403_FORBIDDEN)
 
 
 @permission_classes([IsAdminUser])
