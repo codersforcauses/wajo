86ac2e6ad9bc61ee6eabc9b3a6142c4fcb98d19b
Lok Yx <loklokyiuxuan@gmail.com>
2025-02-24 14:25:14 +0000

    chore(app-sidebar): remove unused import

--- client/src/components/ui/app-sidebar.tsx
+++ client/src/components/ui/app-sidebar.tsx
@@ -2,7 +2,7 @@ import { ArrowLeft, ChevronRight, Command, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 
 import {
   Collapsible,
