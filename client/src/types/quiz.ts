efe815a34d540748e61aada098675b3dae48e3f1
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2025-03-19 17:39:04 +0800

    updated zod schemas used and bit of cleanup

--- client/src/types/quiz.ts
+++ client/src/types/quiz.ts
@@ -163,10 +163,8 @@ export const genericCreateTestSchema = z.object({
     .min(1, "Required")
     .max(255, "Name cannot exceed 255 characters"),
   intro: z.string().min(1, "Required"),
-  // open_time_date: z.date(),
   total_marks: z.number().int().nonnegative().default(0),
   time_limit: z.number().int().nonnegative().default(120),
-  // time_window: z.number().int().nonnegative().default(10),
 });
 
 /**
@@ -180,27 +178,6 @@ export const createCompetitionTestSchema = genericCreateTestSchema.extend({
   open_time_date: z.date(),
   time_window: z.number().int().nonnegative().default(10),
 });
-
-// export const createCompetitionSchema = genericCreateTestSchema
-//   .extend({
-//     start_time: z.date(),
-//     end_time: z.date(),
-//   })
-//   .refine((data) => data.start_time < data.end_time, {
-//     message: "Start time must be earlier than end time.",
-//     path: ["start_time"], // optional: Specify which field the error applies to
-//   });
-
-// export const createPracticeSchema = genericCreateTestSchema.extend({
-//   hours: z
-//     .number()
-//     .min(0, "Number must be at least 0")
-//     .max(24, "Number must be at most 24"),
-//   minutes: z
-//     .number()
-//     .min(0, "Number must be at least 0")
-//     .max(60, "Number must be at most 60"),
-// });
 
 const createQuestionSchema = z.object({
   id: z.number().int().nonnegative(),
