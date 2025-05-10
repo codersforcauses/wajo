d81cc8aa492aba80c2128fb3a26612cc7686e07d
AarifLamat <106091223+AarifLamat@users.noreply.github.com>
2025-05-10 11:27:50 +0800

    updated results page to use data grid instead of boxes with quiz names

---
+++ client/src/components/ui/Results/competition-results-data-grid.tsx
@@ -0,0 +1,167 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as React from "react";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import DateTimeDisplay from "@/components/ui/date-format";
+import DeleteModal from "@/components/ui/delete-modal";
+import { SortIcon } from "@/components/ui/icon";
+import { WaitingLoader } from "@/components/ui/loading";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useDynamicPatchMutation } from "@/hooks/use-put-data";
+import { cn } from "@/lib/utils";
+import { DatagridProps } from "@/types/data-grid";
+import { AdminQuiz } from "@/types/quiz";
+
+/**
+ * Renders a paginated data grid for displaying competition information.
+ *
+ * The `CompetitionDataGrid` component displays a table of competitions with pagination
+ * and sorting functionality. It allows sorting by the competition status and handles
+ * pagination of the data. The data grid updates when changes occur in the data context.
+ *
+ * @function CompetitionDataGrid
+ * @template T - The type of data being displayed in the grid, in this case, `Competition`.
+ * @param {Object} props - The props object.
+ * @param {Competition[]} props.datacontext - The array of competition data items to be displayed in the grid.
+ * @param {function(Competition[]): void} props.onDataChange - Callback triggered when the data changes.
+ * @param {number} props.changePage - The page number to navigate to when the data changes.
+ *
+ * @example
+ *   const [competitions, setCompetitions] = useState<Competition[]>([]);
+ *   const [page, setPage] = useState(1);
+ *
+ *   const handleDataChange = (updatedData: Competition[]) => {
+ *     setCompetitions(updatedData);
+ *   };
+ *
+ *   return (
+ *     <CompetitionDataGrid
+ *       datacontext={competitions}
+ *       onDataChange={handleDataChange}
+ *       changePage={page}
+ *     />
+ *   );
+ */
+export function CompetitionResultsDataGrid({
+  datacontext,
+  isLoading,
+  startIdx,
+  onDeleteSuccess,
+  onOrderingChange = () => {},
+}: DatagridProps<AdminQuiz>) {
+  const router = useRouter();
+
+  const { mutate: setVisible, isPending } = useDynamicPatchMutation({
+    baseUrl: "/quiz/admin-quizzes",
+    queryKeys: [["quiz.admin-quizzes"], ["competition.visible"]],
+    mutationKey: ["competition.visible"],
+    onSuccess: () => {
+      toast.success(`The competition has been updated.`);
+      router.reload();
+    },
+  });
+
+  const onVisible = (id: number, visible: boolean) => {
+    setVisible({
+      id: id,
+      data: { visible },
+    });
+  };
+
+  const commonTableHeadClasses = "w-auto text-white text-nowrap";
+  return (
+    <div className="grid">
+      <div className="overflow-hidden rounded-lg border">
+        <Table className="w-full border-collapse text-left shadow-md">
+          <TableHeader className="bg-black text-lg font-semibold">
+            <TableRow className="hover:bg-muted/0">
+              <TableHead className={commonTableHeadClasses}>No.</TableHead>
+              <TableHead
+                className={commonTableHeadClasses}
+                onClick={() => onOrderingChange("name")}
+              >
+                <SortIcon title="Name" />
+              </TableHead>
+              {/* <TableHead className={commonTableHeadClasses}>Intro</TableHead> */}
+              <TableHead className={commonTableHeadClasses}>
+                Total Marks
+              </TableHead>
+              <TableHead className={commonTableHeadClasses}>
+                Open Date
+              </TableHead>
+              <TableHead className={commonTableHeadClasses}>
+                Time Limit
+              </TableHead>
+              <TableHead className={commonTableHeadClasses}>
+                Time Window
+              </TableHead>
+              <TableHead
+                className={cn(
+                  commonTableHeadClasses,
+                  "sticky right-0 bg-black",
+                )}
+              >
+                Actions
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {!isLoading && datacontext.length > 0 ? (
+              datacontext.map((item, index) => (
+                <TableRow
+                  key={item.id}
+                  className={
+                    "divide-gray-200 border-gray-50 text-sm text-black"
+                  }
+                >
+                  <TableCell className="w-0">
+                    {startIdx ? startIdx + index : item.id}
+                  </TableCell>
+                  <TableCell className="w-1/4">{item.name}</TableCell>
+                  {/* <TableCell className="w-1/2 max-w-80 truncate">
+                    {item.intro}
+                  </TableCell> */}
+                  <TableCell className="w-0">{item.total_marks}</TableCell>
+                  <TableCell className="w-0">
+                    <DateTimeDisplay date={item.open_time_date} />
+                  </TableCell>
+                  <TableCell className="w-0">{item.time_limit}</TableCell>
+                  <TableCell className="w-0">{item.time_window}</TableCell>
+                  <TableCell className="sticky right-0 bg-white">
+                    <Button asChild className="me-1">
+                      <Link href={`${router.pathname}/${item.id}`}>
+                        View Results
+                      </Link>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={8}
+                  className="py-4 text-center text-gray-500"
+                >
+                  {isLoading ? (
+                    <WaitingLoader className="p-0" />
+                  ) : (
+                    "No Results Found"
+                  )}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/dashboard/results/index.tsx
+++ client/src/pages/dashboard/results/index.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
-import { Suspense } from "react";
+import { useRouter } from "next/router";
+import React, { Suspense, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 import { ProtectedPage } from "@/components/layout";
-import { useFetchData } from "@/hooks/use-fetch-data";
-import { AdminQuizNameIdResponse } from "@/types/quiz";
+import { Button } from "@/components/ui/button";
+import {
+  Pagination,
+  PaginationSearchParams,
+  SelectRow,
+  toQueryString,
+} from "@/components/ui/pagination";
+import { CompetitionResultsDataGrid } from "@/components/ui/Results/competition-results-data-grid";
+import { SearchInput } from "@/components/ui/search";
+import { useFetchData, useFetchDataTable } from "@/hooks/use-fetch-data";
+import { pickKeys } from "@/lib/utils";
+import {
+  OrderingItem,
+  orderingToString,
+  stringToOrdering,
+} from "@/types/data-grid";
+import { AdminQuiz, QuizStatus } from "@/types/quiz";
 import { Role } from "@/types/user";
 
 export default function PageConfig() {
@@ -15,31 +32,116 @@ export default function PageConfig() {
   );
 }
 
+type CustomSearchParams = PaginationSearchParams & {
+  status: QuizStatus;
+};
+
 function Index() {
-  const { data, isLoading, error } = useFetchData<AdminQuizNameIdResponse[]>({
-    queryKey: ["quiz.admin-quizzes.quiz_names_and_ids"],
-    endpoint: "/quiz/admin-quizzes/quiz_names_and_ids/",
+  const router = useRouter();
+  const { query, isReady, push } = router;
+
+  const [orderings, setOrderings] = useState<OrderingItem>({});
+
+  const defaultSearchParams: PaginationSearchParams = {
+    ordering: orderingToString(orderings),
+    search: "",
+    nrows: 5,
+    page: 1,
+  };
+
+  const [searchParams, setSearchParams] = useState<CustomSearchParams>({
+    status: QuizStatus.Upcoming,
+    ...defaultSearchParams,
   });
 
-  console.log("data", data);
+  const { data, isLoading, error, totalPages, refetch } =
+    useFetchDataTable<AdminQuiz>({
+      queryKey: ["quiz.admin-quizzes"],
+      endpoint: "/quiz/admin-quizzes/",
+      searchParams: searchParams,
+    });
+
+  useEffect(() => {
+    if (!isLoading) {
+      setSearchParams((prev) => ({
+        ...prev,
+        ordering: (query.ordering as string) || prev.ordering,
+        search: (query.search as string) || prev.search,
+        nrows: Number(query.nrows) || prev.nrows,
+        page: Number(query.page) || prev.page,
+      }));
+      setOrderings(stringToOrdering(query.ordering as string));
+    }
+  }, [query.ordering, query.search, query.nrows, query.page, !isLoading]);
+
+  const setAndPush = (newParams: Partial<CustomSearchParams>) => {
+    const updatedParams = { ...searchParams, ...newParams };
+    setSearchParams(updatedParams);
+
+    const queryParams = pickKeys(
+      updatedParams,
+      ...(Object.keys(defaultSearchParams) as []),
+    );
+    push({ query: toQueryString(queryParams) }, undefined, { shallow: true });
+  };
+
+  const onOrderingChange = (field: keyof OrderingItem) => {
+    setOrderings((prevOrderings) => {
+      const newOrder = prevOrderings[field] === "asc" ? "desc" : "asc";
+      const newOrderings = {
+        [field]: newOrder,
+      } as OrderingItem;
+      setAndPush({ ordering: orderingToString(newOrderings) });
+      return newOrderings;
+    });
+  };
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <div className="flex h-[80vh] flex-col items-center justify-center gap-5">
-      <h2 className="">Quizzes:</h2>
+    <div className="m-4 space-y-4">
+      <div className="flex justify-between">
+        <SearchInput
+          label=""
+          value={searchParams.search ?? ""}
+          placeholder="Search Competition"
+          onSearch={(newSearch: string) => {
+            setAndPush({ search: newSearch, page: 1 });
+          }}
+        />
+      </div>
+
       <Suspense>
-        <div className="flex flex-wrap items-center justify-center gap-10">
-          {data?.map(({ id, name }) => (
-            <div
-              key={id}
-              className="flex h-40 w-40 items-center justify-center rounded-md border-2 border-yellow hover:bg-yellow"
-            >
-              <Link
-                className="flex h-full w-full items-center justify-center text-center text-lg font-semibold"
-                href={`/dashboard/results/${id}`}
-              >
-                {name}
-              </Link>
+        <div>
+          <CompetitionResultsDataGrid
+            datacontext={data ?? []}
+            isLoading={!isReady || isLoading}
+            startIdx={(searchParams.page - 1) * searchParams.nrows + 1}
+            onOrderingChange={onOrderingChange}
+            onDeleteSuccess={refetch}
+          />
+          <div className="flex items-center justify-between p-4">
+            {/* Rows Per Page Selector */}
+            <div className="flex items-center space-x-2">
+              <span className="text-sm text-gray-600">Rows per page:</span>
+              <SelectRow
+                className="h-7 w-20"
+                selectedRow={searchParams.nrows}
+                onChange={(newNrows) =>
+                  setAndPush({ nrows: Number(newNrows), page: 1 })
+                }
+              />
             </div>
-          ))}
+            {/* Pagination Controls */}
+            <Pagination
+              totalPages={totalPages}
+              currentPage={searchParams.page}
+              onPageChange={(newPage: number) =>
+                setAndPush({ page: Math.min(newPage, totalPages) })
+              }
+              className="mr-4 flex"
+            />
+          </div>
         </div>
       </Suspense>
     </div>
